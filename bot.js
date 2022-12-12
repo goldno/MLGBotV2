6eec769c772183ff5c924dc71ee783b365792527
Noah Goldberg <goldno@hotmail.com>
2022-12-12 00:16:40 -0500

    first commit

---
+++ bot.js
@@ -0,0 +1,35 @@
+require("dotenv").config();
+const path = require('node:path');
+const { token } = process.env;
+const { Client, Events, Collection, GatewayIntentBits } = require("discord.js");
+const fs = require("fs");
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+// Initialize commands
+client.commands = new Collection();
+const commandsPath = path.join(__dirname, 'commands');
+const commandFolders = fs.readdirSync(commandsPath);
+for (const folder of commandFolders) {
+  const commandFiles = fs.readdirSync(`${commandsPath}/${folder}`).filter(file => file.endsWith('.js'));
+  for (const file of commandFiles) {
+    const command = require(`${commandsPath}/${folder}/${file}`);
+    client.commands.set(command.data.name, command);
+  }
+}
+
+// Initialize events
+const eventsPath = path.join(__dirname, 'events');
+const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
+
+for (const file of eventFiles) {
+	const filePath = path.join(eventsPath, file);
+	const event = require(filePath);
+	if (event.once) {
+		client.once(event.name, (...args) => event.execute(...args));
+	} else {
+		client.on(event.name, (...args) => event.execute(...args));
+	}
+}
+
+client.login(token);
