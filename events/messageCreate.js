549f4a23c744516adf07d9d49ccbd2573bb080a5
Noah Goldberg <goldno@hotmail.com>
2022-12-21 00:55:43 -0500

    fixed emoji

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -4,8 +4,8 @@ module.exports = {
 	name: Events.MessageCreate,
 	async execute(message) {
 		/* If 2 ICANT emoji are posted, post WECANT emoji */
-        const icantEmoji = '<:ICANT:1054942801240002630>';
-        const wecantEmoji = '<:wecant:1054942811784482867>';
+        const icantEmoji = '<:icant:951864616496017499>';
+        const wecantEmoji = '<:wecant:939359818043514960>';
         await message.channel.messages.fetch({ limit: 2 }).then(messages => {
             const msgs = Array.from(messages.values());
             if(msgs[0].content == icantEmoji && msgs[1].content == icantEmoji) message.channel.send(`${wecantEmoji}`);
