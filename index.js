67aa596f9122fc3fbd6266325d3af90531553bd2
Noah Goldberg <goldno@hotmail.com>
2023-01-05 22:10:54 -0500

    added birthday message cron

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const path = require('node:path');
 const token = process.env.token;
-const { Client, Events, Collection, GatewayIntentBits  } = require("discord.js");
+const { Client, Events, Collection, GatewayIntentBits, EmbedBuilder, AttachmentBuilder  } = require("discord.js");
 const fs = require("fs");
 const cron = require('cron');
 
@@ -44,5 +44,54 @@ const scheduledMessage = new cron.CronJob('00 09 18 * * *', () => {
     channelGeneral.send(`<@${userID}> Hey you sussy baka ${flushedEmoji}, You're looking quite submissive and breedable tonight.`);
 }, null, true, 'America/New_York');
 scheduledMessage.start();
+
+function randomElement(arg) {
+  return arg[Math.floor(Math.random() * arg.length)];
+}
+
+/* MLG Birthday Message */
+const csv = fs.readFileSync('./resources/mlgbirthdays.csv', "utf8");
+const lines = csv.split("\n");
+const headers = lines[0].split(",");
+headers[headers.length - 1] = headers[headers.length - 1].replace("\r", "");
+const birthdays = [];
+for (let i = 1; i < lines.length; i++) {
+  const obj = {};
+  lines[i] = lines[i].replace("\r", "")
+  const currentLine = lines[i].split(",");
+  for (let j = 0; j < headers.length; j++) {
+    obj[headers[j]] = currentLine[j];
+  }
+  birthdays.push(obj);
+}
+const birthdayMessage = new cron.CronJob('00 00 10 * * *', () => {
+  const channelGeneral = client.channels.cache.get('931019066867609690');
+  let currentDate = new Date();
+  let dd = String(currentDate.getDate()).padStart(2, '0');
+  let mm = String(currentDate.getMonth() + 1).padStart(2, '0');
+  currentDate = mm + '/' + dd;
+  birthdays.forEach(member => {
+    if(member.date == currentDate) {
+      const folderPath = './resources/birthdayImages/';
+      fs.readdir(folderPath, (err, files) => {
+        if (err) {
+          console.error(err);
+          return;
+        }
+        const gamerRole = '734586975713099789';
+        const jpgFiles = files.filter(filename => filename.endsWith('.jpg'));
+        const randomImg = randomElement(jpgFiles);
+        const attachment = new AttachmentBuilder(folderPath+randomImg);
+        const embed = new EmbedBuilder()
+          .setTitle('Happy Birthday!')
+          .setDescription(`Happy Birthday to <@${member.id}>!!!!`)
+          .setImage(`attachment://${randomImg}`);
+        channelGeneral.send({ content: `<@&${gamerRole}>`, embeds: [embed], files: [attachment] });
+      });
+    }
+  })
+} , null, true, 'America/New_York');
+birthdayMessage.start();
+
 
 client.login(token);
