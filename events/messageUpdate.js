6c19138ccb76b62448e026ca07baeb13dda29df7
Noah Goldberg <goldno@hotmail.com>
2023-05-18 17:47:34 -0400

    fixed

--- events/messageUpdate.js
+++ events/messageUpdate.js
@@ -8,7 +8,7 @@ module.exports = {
         const leagueRole = '<@&1096596027739480125>'
         const channel = oldMessage.channel;
         const userID = oldMessage.author.id;
-        if(newMessage.content.includes(testRole)) {
+        if(newMessage.content.includes(leagueRole)) {
             newMessage.delete()
                 .then(msg => channel.send(`<@${userID}> This command does not exist (good try XD)`))
                 .catch(console.error);
