bbe1a06b17d6751e2b1e98379df29838dd36ebfd
Noah Goldberg <goldno@hotmail.com>
2023-05-20 23:55:12 -0400

    fixed icant

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -35,7 +35,7 @@ module.exports = {
         }
 
 		/* If 2 ICANT emoji are posted, post WECANT emoji */
-        const icantEmoji = '<:icant:927771695450828840>';
+        const icantEmoji = '<:icant:1081267987299975208>';
         const wecantEmoji = '<:wecant:939359818043514960>';
         await message.channel.messages.fetch({ limit: 2 }).then(messages => {
             const msgs = Array.from(messages.values());
