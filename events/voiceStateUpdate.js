263bc07e8fea47b286cf874a4a8e355a3c1fd3ff
Noah Goldberg <goldno@hotmail.com>
2024-01-22 19:35:46 -0500

    t

--- events/voiceStateUpdate.js
+++ events/voiceStateUpdate.js
@@ -5,11 +5,11 @@ const watchPartyID = "1180248299819778058"
 module.exports = {
 	name: Events.VoiceStateUpdate,
 	async execute(oldMember, newMember) {
-        //console.log(oldMember);
-        //console.log(newMember);
+        // console.log(oldMember);
+        // console.log(newMember);
 
-        // let newUserChannelID = newMember.channelId;
-        // let oldUserChannelID = oldMember.channelId;
+        let newUserChannelID = newMember.channelId;
+        let oldUserChannelID = oldMember.channelId;
 
         // if(newUserChannelID === generalTestID) {
         //     console.log("User joined voice channel!");
@@ -19,6 +19,21 @@ module.exports = {
 		// 	.setTitle('Watch Party! (shhhh)');
         // } else {
         //     //console.log("User left voice channel!");
+        // }
+
+        /* Sour patch kid cheese touch */
+        // const prunedChannelID = "1117631591364182086";
+        // const testGeneralID = "618902972700819469";
+        // const generalVoiceChannelID = "1158252699704381481";
+        // const SPKidID = "90182872316510208";
+
+        // const members = await newMember.guild.members.fetch();
+        // const randMember = members.random();
+
+        // if(newUserChannelID = testGeneralID) {
+        //     if(newMember.channel.members.find(member => member.user.id === SPKidID).user.id =) {
+        //         console.log("This is the sp kid!")
+        //     }
         // }
 	},
 };
